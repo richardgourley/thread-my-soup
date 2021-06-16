fe882451884f1a8cd1156ceca48e04a310053460
Rich <codeleap@outlook.com>
2021-06-16 11:05:53 +0200

    Corrected to ask_user_for_elemet

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ url_selector = UrlSelector()
 
 # get url selection AND element to search from the user
 urls = url_selector.ask_user_for_urls()
-element_to_search = url_selector.ask_user_element_to_search()
+element_to_search = url_selector.ask_user_for_element()
 
 element_getter = ElementGetter(urls, element_to_search)
 
-element_getter.create_start_threads()
+element_getter.start_threads()
 element_getter.clear_files_content()
 
 print(f"The results can be found in the file: {element_getter.results_file}")

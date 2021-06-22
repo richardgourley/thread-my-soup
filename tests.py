1c9756a2b6152bc3d559da6fa46db031d5b1e699
Rich <codeleap@outlook.com>
2021-06-22 11:30:41 +0200

    If __name__ == "__main__" run unittest.main() added

--- tests.py
+++ tests.py
@@ -2,8 +2,17 @@ import unittest
 from elementretriever import ElementRetriever
 from searchsetup import SearchSetUp
 import threadmysoup
+import os
 
 class UnitTest(unittest.TestCase):
 	@classmethod
 	def setUp(self):
+		print("UnitTest setup")
 		pass
+
+	def test_example_1(self):
+		print("I am test example 1")
+		self.assertTrue(5 * 5 == 23)
+
+if __name__ == "__main__":
+	unittest.main()

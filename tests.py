f8f5a1318df820c2de7a55cb7f54e750195fc139
Rich <codeleap@outlook.com>
2021-06-22 09:54:32 +0200

    Tests file added

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+from elementretriever import ElementRetriever
+from searchsetup import SearchSetUp
+import threadmysoup
+
+class UnitTest(unittest.TestCase):
+	@classmethod
+	def setUp(self):
+		pass

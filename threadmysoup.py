17f1b497590eafc03eee38a94df795e1b73a9ce4
Rich <codeleap@outlook.com>
2021-08-20 12:49:30 +0200

    Indentation changed ;

--- threadmysoup.py
+++ threadmysoup.py
@@ -1,26 +1,54 @@
-class ThreadMySoup:
+from classes.class1 import Class1
+
+class_1 = Class1()
+class_1.say_hello()
+
+class ThreadMySoupSetUp:
 	def __init__(self):
 		pass
 
-	def get_command_line_args(self):
+	def get_command_line_html_element_args(self):
 		if __name__ == "__main__":
 			import sys
 
-			accepted_element_arguments = ('h1','h2','h3','h4','h5','p','a','ul','li','span','title',)
-			number_arguments = len(sys.argv)
+			#accepted_element_arguments = ('h1','h2','h3','h4','h5','p','a','ul','li','span','title',)
+			
+			command_line_args = list(sys.argv)
 
 			# First argument is always the current file...
+			command_line_args.pop(0)
 
-			if number_arguments == 1:
+			number_arguments = len(command_line_args)
+
+			if number_arguments == 0:
 				self.print_no_arguments_added()
 				quit()
 
-			if number_arguments > 1:
+			if number_arguments > 0:
 				print("You have chosen to search these elements:")
-				for i in range(1, number_arguments):
-					print(sys.argv[i])
+				for arg in command_line_args:
+					print(arg)
 
-				return sys.argv
+				return command_line_args
+
+	def ask_user_for_urls_to_search(self):
+		urls = []
+
+		still_entering_urls = True
+		
+		while still_entering_urls:
+			print("Enter a url to find the html elements in.")
+			url = input()
+			if url == "":
+				print("Sorry, url can't be blank.")
+				continue
+			urls.append(url)
+			print("Enter another url? Enter 'Y' or 'N':")
+			enter_another = input()
+			if enter_another == 'N' or enter_another == 'n':
+				still_entering_urls = False
+
+		return urls
 
 	def print_no_arguments_added(self):
 		print("You haven't added any arguments.")
@@ -28,5 +56,14 @@ class ThreadMySoup:
 		print("EXAMPLE: 'threadmysoup.py h1 h2 h3 p a'")
 
 
-threadmysoup = ThreadMySoup()
-print(threadmysoup.get_command_line_args())
+thread_my_soup = ThreadMySoupSetUp()
+html_elements_to_find = thread_my_soup.get_command_line_html_element_args()
+
+print(html_elements_to_find)
+
+urls_to_search = thread_my_soup.ask_user_for_urls_to_search()
+
+print(urls_to_search)
+
+## Pass 'html_elements_to_find' and 'urls_to_search' to another class, that will perform 
+## ... the searches of html elements for us

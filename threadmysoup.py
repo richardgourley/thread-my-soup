b2b047576b41c22782c8060111d33c9ba4c48236
Rich <codeleap@outlook.com>
2021-08-20 10:47:02 +0200

    Rewritten as main program class with get_command_line_args added

--- threadmysoup.py
+++ threadmysoup.py
@@ -1,32 +1,30 @@
-from run import run
+class ThreadMySoup:
+	def __init__(self):
+		pass
 
-def print_arguments_incorrect_message():
-	print("Please run 'python3 threadmysoup.py' + 1 argument:")
-	print("'python3 threadmysoup.py p' + 1 of these arguments:")
-	print("'h1','h2','h3','h4','h5','p','ul','li','span','title'")
+	def get_command_line_args(self):
+		if __name__ == "__main__":
+			import sys
 
-if __name__ == "__main__":
-	import sys
+			accepted_element_arguments = ('h1','h2','h3','h4','h5','p','a','ul','li','span','title',)
+			number_arguments = len(sys.argv)
 
-	accepted_arguments = ('h1','h2','h3','h4','h5','p','ul','li','span','title',)
+			# First argument is always the current file...
 
-	### sys.argv is a list of params passed by user 'python3 threadmysoup.py p' etc.
-	### 1st item is always file name --> ['threadmysoup.py'...other args...]
-	number_arguments = len(sys.argv)
+			if number_arguments == 1:
+				self.print_no_arguments_added()
+				quit()
 
-	if number_arguments == 1:
-		print("One argument required")
-		print_arguments_incorrect_message()
-		quit()
+			if number_arguments > 1:
+				print("You have chosen to search these elements:")
+				for i in range(1, number_arguments):
+					print(sys.argv[i])
 
-	if number_arguments > 2:
-		print("Too many arguments")
-		print_arguments_incorrect_message()
-		quit()
+	def print_no_arguments_added(self):
+		print("You haven't added any arguments.")
+		print("Please run threadmysoup.py + 1 or more elements to search separated by a space.")
+		print("EXAMPLE: 'threadmysoup.py h1 h2 h3 p a'")
 
-	if number_arguments == 2:
-		if sys.argv[1] in accepted_arguments:
-			### proceed with program
-			run(sys.argv[1])
-		else:
-			print("Incorrect argument entered")
+
+threadmysoup = ThreadMySoup()
+threadmysoup.get_command_line_args()

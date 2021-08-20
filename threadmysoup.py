681db0d4687eb4c305441914f699838f8245b235
Rich <codeleap@outlook.com>
2021-08-20 12:50:57 +0200

    Indentation changed back

--- threadmysoup.py
+++ threadmysoup.py
@@ -4,56 +4,56 @@ class_1 = Class1()
 class_1.say_hello()
 
 class ThreadMySoupSetUp:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def get_command_line_html_element_args(self):
-		if __name__ == "__main__":
-			import sys
+    def get_command_line_html_element_args(self):
+        if __name__ == "__main__":
+            import sys
 
-			#accepted_element_arguments = ('h1','h2','h3','h4','h5','p','a','ul','li','span','title',)
-			
-			command_line_args = list(sys.argv)
+            #accepted_element_arguments = ('h1','h2','h3','h4','h5','p','a','ul','li','span','title',)
+            
+            command_line_args = list(sys.argv)
 
-			# First argument is always the current file...
-			command_line_args.pop(0)
+            # First argument is always the current file...
+            command_line_args.pop(0)
 
-			number_arguments = len(command_line_args)
+            number_arguments = len(command_line_args)
 
-			if number_arguments == 0:
-				self.print_no_arguments_added()
-				quit()
+            if number_arguments == 0:
+                self.print_no_arguments_added()
+                quit()
 
-			if number_arguments > 0:
-				print("You have chosen to search these elements:")
-				for arg in command_line_args:
-					print(arg)
+            if number_arguments > 0:
+                print("You have chosen to search these elements:")
+                for arg in command_line_args:
+                    print(arg)
 
-				return command_line_args
+                return command_line_args
 
-	def ask_user_for_urls_to_search(self):
-		urls = []
+    def ask_user_for_urls_to_search(self):
+        urls = []
 
-		still_entering_urls = True
-		
-		while still_entering_urls:
-			print("Enter a url to find the html elements in.")
-			url = input()
-			if url == "":
-				print("Sorry, url can't be blank.")
-				continue
-			urls.append(url)
-			print("Enter another url? Enter 'Y' or 'N':")
-			enter_another = input()
-			if enter_another == 'N' or enter_another == 'n':
-				still_entering_urls = False
+        still_entering_urls = True
+        
+        while still_entering_urls:
+            print("Enter a url to find the html elements in.")
+            url = input()
+            if url == "":
+                print("Sorry, url can't be blank.")
+                continue
+            urls.append(url)
+            print("Enter another url? Enter 'Y' or 'N':")
+            enter_another = input()
+            if enter_another == 'N' or enter_another == 'n':
+                still_entering_urls = False
 
-		return urls
+        return urls
 
-	def print_no_arguments_added(self):
-		print("You haven't added any arguments.")
-		print("Please run threadmysoup.py + 1 or more elements to search separated by a space.")
-		print("EXAMPLE: 'threadmysoup.py h1 h2 h3 p a'")
+    def print_no_arguments_added(self):
+        print("You haven't added any arguments.")
+        print("Please run threadmysoup.py + 1 or more elements to search separated by a space.")
+        print("EXAMPLE: 'threadmysoup.py h1 h2 h3 p a'")
 
 
 thread_my_soup = ThreadMySoupSetUp()

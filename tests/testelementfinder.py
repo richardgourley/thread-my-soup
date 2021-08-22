a17de407bc9fb67fe9410ddeaedc850b91c64b7f
Rich <codeleap@outlook.com>
2021-08-22 11:18:18 +0200

    Tests to follow note added to file

---
+++ tests/testelementfinder.py
@@ -0,0 +1,14 @@
+import unittest
+from threading import Thread, Lock
+import os
+
+from classes import elementfinder
+
+class UnitTest(unittest.TestCase):
+	@classmethod
+	def setUp(self):
+		'''
+		Tests to follow shortly.
+		Class recently refactored...
+		'''
+		pass

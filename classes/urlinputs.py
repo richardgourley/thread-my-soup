a2007aefe8c29692c97c3937fe1cf6d07d537eaf
Rich <codeleap@outlook.com>
2021-08-24 14:34:50 +0200

    New args, searcher and inputs classes added

---
+++ classes/urlinputs.py
@@ -0,0 +1,13 @@
+from classes.base.userinputbase import UserInputBase
+
+class UrlInputs(UserInputBase):
+	def __init__(self, 
+		enter_input_message = "Enter a url to search. Press 'Q' or 'q' to stop entering urls.", 
+		blank_input_message = "The url you enter must not be blank"
+	):
+		super(UrlInputs, self).__init__(
+			enter_input_message,
+			blank_input_message 
+		)
+
+		

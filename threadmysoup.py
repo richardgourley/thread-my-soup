4dfd9bd8ba02df62f94922c9bb66bbce849c3a9d
Rich <codeleap@outlook.com>
2021-08-24 14:33:37 +0200

    Main file changed - pulls in other classes instead of ThreadMySoupSetup class

--- threadmysoup.py
+++ threadmysoup.py
@@ -1,63 +1,21 @@
-from classes.elementfinder import ElementFinder
-import tests
-
-class ThreadMySoupSetUp:
-	def __init__(self):
-		pass
-
-	def get_command_line_html_element_args(self):
-		command_line_args = list(sys.argv)
-
-		# Remove first arg -> because first arg is always the current file name
-		command_line_args.pop(0)
-
-		number_arguments = len(command_line_args)
-
-		if number_arguments == 0:
-			self.print_no_command_line_arguments_added()
-			quit()
-
-		if number_arguments > 0:
-			print("You have chosen to search these elements:")
-			for arg in command_line_args:
-				print(arg)
-
-			return command_line_args
-
-	def ask_user_for_urls_to_search(self):
-		urls = []
-
-		still_entering_urls = True
-		
-		while still_entering_urls:
-			print("Enter a url to find the html elements in.")
-			url = input()
-			if url == "":
-				print("Sorry, url can't be blank.")
-				continue
-			urls.append(url)
-			print("Enter another url? Enter 'Y' or 'N':")
-			enter_another = input()
-			if enter_another == 'N' or enter_another == 'n':
-				still_entering_urls = False
-
-		return urls
-
-	def print_no_command_line_arguments_added(self):
-		print("You haven't added any html element arguments!")
-		print("Please run threadmysoup.py + 1 or more elements to search separated by a space.")
-		print("EXAMPLE: 'threadmysoup.py h1 h2 h3 p a'")
+from classes.htmltagargs import HtmlTagArgs
+from classes.urlinputs import UrlInputs
+from classes.htmltagsearcher import HtmlTagSearcher
 
 if __name__ == "__main__":
-	thread_my_soup = ThreadMySoupSetUp()
+	html_tag_args = HtmlTagArgs()
+
+	html_tags = html_tag_args.return_html_tag_args()
+
+	url_inputs = UrlInputs()
+
+	urls = url_inputs.ask_user_for_inputs()
 	
-	html_elements_to_find = thread_my_soup.get_command_line_html_element_args()
-	urls_to_search = thread_my_soup.ask_user_for_urls_to_search()
+	## pass html_tag_args, urls and file names to html_tag_searcher to run program
+	html_tag_searcher = HtmlTagSearcher(html_tags, urls, "temp.txt", "results.txt")
 
-	element_finder = ElementFinder(
-			html_elements_to_find,
-			urls_to_search
-		)
 
-	element_finder.start_threads()
 
+	 
+
+	

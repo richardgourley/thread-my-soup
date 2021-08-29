e0ec08f6dfa11f649e1a6bb4d60af76760dc6383
Rich <codeleap@outlook.com>
2021-08-29 10:52:34 +0200

    Menu options created as dictionary. Arg has to be found in dictionary to continue program

--- classes/args.py
+++ classes/args.py
@@ -1,47 +1,53 @@
 from classes.helper.commandlineargs import CommandLineArgs
 
 class Args:
-	def __init__(self):
-		self.command_line_args = CommandLineArgs()
+    def __init__(self):
+        self.command_line_args = CommandLineArgs()
 
-	def return_args(self):
-		# removes 1st element - file name arg
-		args = self.command_line_args.get_command_line_args()
+    def return_args(self):
+        # removes 1st element - file name arg
+        args = self.command_line_args.get_command_line_args()
 
-		menu_args = ["searchwords", "searchidsorclasses", "searchhtmltags"]
+        menu_options = [
+            {
+                "arg":"searchwords",
+                "full_name":"search for words",
+            },
+            {
+                "arg":"searchidsorclasses",
+                "full_name":"search ids or classes",
+            },
+            {
+                "arg":"searchhtmltags",
+                "full_name":"search html tags",
+            },
+        ]
 
-		menu_args = {
-			{
-				"arg":"searchwords",
-				"full_name":"search for words"
-			},
-			{
-				"arg":"searchidsorclasses",
-				"full_name":"search ids or classes"
-			},
-			{
-				"arg":"searchhtmltags",
-				"full_name":"search html tags"
-			},
-		}
+        if len(args) == 0 or len(args) > 1:
+            self.args_incorrect_message()
+            quit()
 
-		if len(args) == 0:
-			self.args_incorrect_message()
-			quit()
+        menu_option = self.get_menu_option(args[0], menu_options)
 
-		if len(args) > 1:
-			self.args_incorrect_message()
-			quit()
+        if menu_option is None:
+            self.args_incorrect_message()
+            quit() 
 
-		if len(args) == 1 and args[0] in menu_args:
-			print(f"You have chosen to {args[0]}")
+        print(menu_option["full_name"])
 
-		return args
+        return args
 
-	def args_incorrect_message(self):
-		print("You need to run'threadmysoup.py' + 1 of the following menu options")
-		print("EXAMPLES:")
-		print("threadmysoup.py searchwords (find any words in the text)")
-		print("threadmysoup.py searchidsorclasses (find elements with specific ids or classes)")
-		print("threadmysoup.py searchhtmltags (find all html tags)")
+    def get_menu_option(self, arg, menu_options):
+        for option in menu_options:
+            if option["arg"] == arg:
+                return option
+
+        return None
+
+    def args_incorrect_message(self):
+        print("You need to run'threadmysoup.py' + 1 of the following menu options")
+        print("EXAMPLES:")
+        print("threadmysoup.py searchwords (find any words in the text)")
+        print("threadmysoup.py searchidsorclasses (find elements with specific ids or classes)")
+        print("threadmysoup.py searchhtmltags (find all html tags)")
 

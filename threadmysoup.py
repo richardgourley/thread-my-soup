c0372e46a5fb66843885ad3e8134bea97db4d3d5
Rich <codeleap@outlook.com>
2021-08-29 11:24:55 +0200

    New class names refactored into main file

--- threadmysoup.py
+++ threadmysoup.py
@@ -1,5 +1,5 @@
 from classes.args import Args
-from classes.menuinputs import MenuInputs
+from classes.iteminputs import ItemInputs
 from classes.urlinputs import UrlInputs
 from classes.filesearcher import FileSearcher
 from classes.urlsearcher import UrlSearcher
@@ -9,16 +9,15 @@ if __name__ == "__main__":
 	return_args = args.return_args()
 	menu_option = return_args[0]
 
-	menu_inputs = MenuInputs(menu_option)
-	items_to_search_for = menu_inputs.ask_user_for_inputs()
+	item_inputs = ItemInputs(menu_option)
+	items_to_search_for = item_inputs.ask_user_for_inputs()
 
-	search_urls_or_files = menu_inputs.ask_user_for_preference(["files", "urls"], "Would you like to enter urls or search through html files?")
+	search_urls_or_files = item_inputs.ask_user_for_preference(["files", "urls"], "Would you like to enter urls or search through html files?")
 
 	if search_urls_or_files == "files":
 		file_searcher = FileSearcher(
 			menu_option,
-			items_to_search_for,
-			"results.txt"
+			items_to_search_for
 		)
 	else:
 		url_inputs = UrlInputs()
@@ -26,7 +25,5 @@ if __name__ == "__main__":
 		url_searcher = UrlSearcher(
 			menu_option,
 			items_to_search_for,
-			urls_to_search,
-			"temp.txt",
-			"results.txt"
+			urls_to_search
 		)

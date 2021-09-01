0db643cdc4a9e29d0ef49f62024f88b2f2d7bacd
Rich <codeleap@outlook.com>
2021-09-01 15:53:00 +0200

    ElementFinder class tests added

---
+++ tests/testelementfinder.py
@@ -0,0 +1,32 @@
+import unittest
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
+
+from classes.helper.elementfinder import ElementFinder
+
+class UnitTest(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.element_finder = ElementFinder()
+        self.unittest_file = "tests/unittest.html"
+
+    def test_find_html_elements(self):
+        soup = None
+        with open(self.unittest_file, 'r') as file:
+            soup = BeautifulSoup(file.read(), "html.parser")
+        html_elements_to_find = ['p','li']
+        soup_result_list = self.element_finder.find_html_elements(soup, html_elements_to_find)
+        self.assertEqual(len(soup_result_list), 2)
+        self.assertEqual(type(soup_result_list), list)
+        self.assertEqual(len(soup_result_list[0]), 1)
+        self.assertEqual(soup_result_list[0][0].get_text(), 'Hello, world!')
+        self.assertEqual(soup_result_list[0][0].name, 'p')
+        self.assertEqual(soup_result_list[1][0].get_text(), 'Section 1')
+        self.assertEqual(soup_result_list[1][0].name, 'li')
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()

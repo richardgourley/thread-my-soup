b1ef06ad97768d969b2a228bb6c1d0267c322227
Rich <codeleap@outlook.com>
2021-09-07 11:45:57 +0200

    Arg menu options printed from options directory - code can be re-used.

--- classes/args.py
+++ classes/args.py
@@ -1,53 +1,39 @@
 from classes.helper.commandlineargs import CommandLineArgs
 
 class Args:
-    def __init__(self):
+    def __init__(self, options):
         self.command_line_args = CommandLineArgs()
+        self.options = options
 
-    def filter_args(self):
+    def return_menu_option_or_print_args_incorrect(self):
         # removes 1st element - file name arg
         args = self.command_line_args.get_command_line_args()
-
-        menu_options = [
-            {
-                "arg":"searchwords",
-                "full_name":"search for words",
-            },
-            {
-                "arg":"searchidsorclasses",
-                "full_name":"search ids or classes",
-            },
-            {
-                "arg":"searchhtmltags",
-                "full_name":"search html tags",
-            },
-        ]
 
         if len(args) == 0 or len(args) > 1:
             self.args_incorrect_message()
             quit()
 
-        menu_option = self.get_menu_option(args[0], menu_options)
+        menu_option = self.get_menu_option(args[0], self.options)
 
         if menu_option is None:
             self.args_incorrect_message()
             quit() 
 
-        print(f"You have chosen to: {menu_option['full_name']}")
+        print(f"You have chosen to: {menu_option['arg_full_name']}")
         
-        return args
+        return menu_option
 
-    def get_menu_option(self, arg, menu_options):
-        for option in menu_options:
-            if option["arg"] == arg:
+    def get_menu_option(self, arg, options):
+        for option in options:
+            if option["arg_name"] == arg:
                 return option
 
         return None
 
     def args_incorrect_message(self):
-        print("You need to run'threadmysoup.py' + 1 of the following menu options")
+        print("You need to run 'python3' + 1 of the following menu options")
         print("EXAMPLES:")
-        print("threadmysoup.py searchwords (find any words in the text)")
-        print("threadmysoup.py searchidsorclasses (find elements with specific ids or classes)")
-        print("threadmysoup.py searchhtmltags (find all html tags)")
+        for arg_menu_option in self.options:
+            print(f"threadmysoup.py {arg_menu_option['arg_name']} {arg_menu_option['arg_menu_description']}")
+        print("----------")
 

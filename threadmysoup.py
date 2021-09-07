82694bc37c6f5f8b6e5a841c971c5e8fa8800fc4
Rich <codeleap@outlook.com>
2021-09-07 11:36:15 +0200

    Menu options - arg menu description added

--- threadmysoup.py
+++ threadmysoup.py
@@ -1,29 +1,71 @@
 from classes.args import Args
-from classes.iteminputs import ItemInputs
-from classes.urlinputs import UrlInputs
+from classes.maininputsmenu import MainInputsMenu
+from classes.userinput import UserInput
+from classes.directoryinputsmenu import DirectoryInputsMenu
 from classes.filesearcher import FileSearcher
 from classes.urlsearcher import UrlSearcher
+from classes.helper.elementfinder import ElementFinder
 
 if __name__ == "__main__":
-	args = Args()
-	filtered_args= args.filter_args()
-	menu_option = filtered_args[0]
+	element_finder = ElementFinder()
 
-	item_inputs = ItemInputs(menu_option)
-	items_to_search_for = item_inputs.ask_user_for_inputs()
+	menu_options = [
+		{
+			'arg_name':'searchwords',
+			'arg_full_name':'Search Words',
+			'arg_menu_description':'(find matching words in the text of elements)',
+			'enter_input_message':"Enter a word to search in the text. Press 'Q' or 'q' to stop entering words.",
+			'blank_input_message':"The word you enter cannot be blank.",
+			'search_function':element_finder.find_words
+		},
+		{
+			'arg_name':'searchidsorclasses',
+			'arg_full_name':'Search Ids or Classes',
+			'arg_menu_description':'(find elements with matching ids or classes)',
+			'enter_input_message':"Enter a class or id to search for. Press 'Q' or 'q' to stop entering ids or classes.",
+			'blank_input_message':"The class or id name you enter cannot be blank.",
+			'search_function':element_finder.find_ids_or_classes
+		},
+		{
+			'arg_name':'searchhtmltags',
+			'arg_full_name':'Search HTML tags',
+			'arg_menu_description':'(find all matching html tags)'
+			'enter_input_message':"Enter an html tag to search for. Press 'Q' or 'q' to stop entering html elements.",
+			'blank_input_message':"The html tag name you enter cannot be blank.",
+			'search_function':element_finder.find_html_elements
+		}
+	]
 
-	search_urls_or_files = item_inputs.ask_user_for_preference(["files", "urls"], "Would you like to enter urls or search through html files?")
+	# Args menu - search words, search ids or classes, search html tags?
+	args = Args(menu_options)
+	menu_option = args.return_menu_option_or_print_args_incorrect()
 
-	if search_urls_or_files == "files":
+	# Which items to search
+	user_input = UserInput(menu_option)
+	items_to_search_for = user_input.ask_user_for_inputs()
+
+	# Option - Search the 'files' dir OR traverse another directory
+	search_files_dir_or_other_dir = user_input.ask_user_for_preference(
+		["files", "other"], 
+		"Enter an option below - would you like to search through all files and sub-directories in the 'files' directory or search through a different directory on your system?\nEnter 'files directory or 'other directory'"
+	)
+
+	if search_files_dir_or_other_dir == "files":
 		file_searcher = FileSearcher(
 			menu_option,
-			items_to_search_for
+			items_to_search_for,
 		)
 	else:
-		url_inputs = UrlInputs()
-		urls_to_search = url_inputs.ask_user_for_inputs()
-		url_searcher = UrlSearcher(
+		directory_input_menu = UserInput(
+			{
+				"enter_input_message":"Enter a directory to search.\nThe directory should start from your system root and with a slash eg. '/home/user/webprojects/portfoliosite", 
+				"blank_input_message":"The directory you enter must not be blank"
+			}
+		)
+		directory_to_search = directory_input_menu.ask_user_for_input()
+		
+		file_searcher = FileSearcher(
 			menu_option,
 			items_to_search_for,
-			urls_to_search
+			directory_to_search
 		)

bea62ba6adf856302a46b8ebd5649c8806ad7621
Rich <codeleap@outlook.com>
2021-10-13 15:46:28 +0200

    Tests for validate_directory added

--- tests/testdirectoryvalidation.py
+++ tests/testdirectoryvalidation.py
@@ -6,11 +6,15 @@ class UnitTest(unittest.TestCase):
 	@classmethod
 	def setUp(self):
 		self.directory_validation = DirectoryValidation()
-		current_dir = os.getcwd()
+		self.current_dir = os.getcwd()
 
-	def test_incorrect_dir_returns_false(self):
+	def test_validate_directory_returns_false(self):
 		is_dir_valid = self.directory_validation.validate_directory("/home/incorrect/path/testing")
 		self.assertFalse(is_dir_valid)
+
+	def test_validate_directory_returns_true(self):
+		is_dir_valid = self.directory_validation.validate_directory(self.current_dir)
+		self.assertTrue(is_dir_valid)
 
 if __name__ == "__main__":
 	unittest.main()

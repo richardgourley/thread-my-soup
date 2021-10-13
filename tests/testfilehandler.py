03de1cff7d96f903a95b79ee40cc75984ce53411
Rich <codeleap@outlook.com>
2021-10-13 15:45:50 +0200

    Tests for parse file added

--- tests/testfilehandler.py
+++ tests/testfilehandler.py
@@ -1,43 +1,23 @@
 import unittest
-from bs4 import BeautifulSoup
-from datetime import datetime
 import os
-
-from classes.helper.filehandler import FileHandler
+from classes.filehandler import FileHandler
 
 class UnitTest(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.file_handler = FileHandler()
-        self.unittest_file = 'tests/unittest.html'
-        with open(self.unittest_file, 'w') as file:
+        with open('unittest.html', 'w') as file:
             file.write(
                 "<p class='text-secondary'>Hello, world!</p>\n<ul><li>Section 1</li></ul>"
             )
 
-    def test_parse_file_returns_beautifulsoup_instance(self):
-        soup = self.file_handler.parse_file(self.unittest_file)
-        self.assertEqual(type(soup),  BeautifulSoup)
+    def test_parse_file_returns_soup(self):
+        file_name = 'unittest.html'
+        soup = self.file_handler.parse_file(file_name)
+        paragraphs = soup.find_all('p')
+        self.assertTrue(soup, 'find_all')
+        self.assertEqual(str(paragraphs[0].getText()), "Hello, world!")
 
-    def test_create_timestamped_results_file_returns_string(self):
-        date_now = datetime.now()
-        results_file = self.file_handler.create_timestamped_results_file()
-        self.assertIs(type(results_file), str)
-
-    def test_create_timestampled_results_file_contains_todays_date(self):
-        date_now = datetime.now()
-        results_file = self.file_handler.create_timestamped_results_file()
-        self.assertTrue(str(date_now.hour) in results_file)
-        self.assertTrue(str(date_now.minute) in results_file)
-        self.assertTrue(str(date_now.day) in results_file)
-        self.assertTrue(str(date_now.month) in results_file)
-
-    def test_clear_temp_file_works(self):
-        self.file_handler.clear_temp_file()
-        opened_file = ""
-        with open('temp.txt', 'r') as file:
-            opened_file = file.read()
-        self.assertEqual(len(opened_file), 0)
 
 if __name__ == "__main__":
     unittest.main()
